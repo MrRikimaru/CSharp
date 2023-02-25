c128a866b9e02e3358aacd7614ee585988e550c7
Berg Andrey <berg.andrey@icloud.com>
2023-02-25 03:25:46 +0300

    Add Task 00

---
+++ Task 00/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 00. Ввод числа с клавиатуры и вывод его значения в квадрате.
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number*number;
+Console.Write($"Квадрат числа {number} = {square}");
