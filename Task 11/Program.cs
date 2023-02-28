67667b43209fa89cd63bd57a8b0a7460159bd1a5
Berg Andrey <berg.andrey@icloud.com>
2023-02-28 17:17:15 +0300

    Add Task 11

---
+++ Task 11/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int randNumber = new Random().Next(100, 1000);
+int result = (randNumber / 100) * 10 + (randNumber % 10);
+Console.Write($"{randNumber} -> {result}.");
