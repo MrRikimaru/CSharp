06215189b965f12d81fb1ceba6fbf335c532f257
Berg Andrey <berg.andrey@icloud.com>
2023-02-28 17:10:57 +0300

    Add Task 09

---
+++ Task 09/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+int randNumber = new Random().Next(10, 100);
+if (randNumber / 10 > randNumber % 10) Console.Write($"{randNumber} -> {randNumber / 10}");
+else Console.Write($"{randNumber} -> {randNumber % 10}");
