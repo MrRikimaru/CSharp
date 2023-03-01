b4b8108ecab2c70599ba842a9ac9b106926ae51d
Berg Andrey <berg.andrey@icloud.com>
2023-03-01 21:52:09 +0300

    GuessTheNumber Edited

--- Games/GuessTheNumber/Program.cs
+++ Games/GuessTheNumber/Program.cs
@@ -1,4 +1,4 @@
-﻿int score = 1000, fivePrecent = 0;
+﻿double score = 1000, fivePercent = 0;
 Console.Write("Введите начало интервала случайных чисел:");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите конец интервала случайных чисел:");
@@ -7,18 +7,18 @@ int randNmbr = new Random().Next(min, max + 1);
 Console.WriteLine($"{randNmbr}.");
 while (true)
 {
-    fivePrecent = score / 100 * 5;
+    fivePercent = score / 100 * 5;
     Console.Write("Введите число:");
     int nmbr = Convert.ToInt32(Console.ReadLine());
     if (nmbr > randNmbr)
     {
         Console.WriteLine("Ваше число больше загаданного.");
-        score = score - fivePrecent;
+        score = score - fivePercent;
     } 
     else if (nmbr < randNmbr)
     {
         Console.WriteLine("Ваше число меньше загаданного.");
-        score = score - fivePrecent;
+        score = score - fivePercent;
     } 
     else 
     {
