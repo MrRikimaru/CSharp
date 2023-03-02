2a9c38811e9bead9309324e61becc2d30364d340
Berg Andrey <berg.andrey@icloud.com>
2023-03-02 20:36:07 +0300

    Add Task 18

---
+++ Task 18/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 18: Напишите программу, которая по
+//заданному номеру четверти, показывает диапазон
+//возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+string result = ((quarter > 0) && (quarter< 4))
+                        ? $"Указанной четверти соответсвуют следующий диапозон - {Quarter(quarter)}" 
+                        : "Введены некорректрные координаты";
+Console.Write(result);
+
+string Quarter(int quarter_value)
+{
+    if (quarter_value == 1) return "X > 0, Y > 0";
+    if (quarter_value == 2) return "X < 0, Y > 0";
+    if (quarter_value == 3) return "X < 0, Y < 0";
+    if (quarter_value == 4) return "X > 0, Y < 0";
+    return "0";
+}
