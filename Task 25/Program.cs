acea1d4cd96212704fb0ec54053e1dee5f8fbd2f
Berg Andrey <berg.andrey@icloud.com>
2023-03-06 22:53:04 +0300

    Task 25 Edited

--- Task 25/Program.cs
+++ Task 25/Program.cs
@@ -7,17 +7,11 @@ int nmbrA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B:");
 int nmbrB = Convert.ToInt32(Console.ReadLine());
 if (nmbrB < 0) Console.WriteLine("Число B не может быть отрицательным!");
-else
-{
-    Console.Write($"{nmbrA} в {nmbrB} степени = {Exponentiation(nmbrA, nmbrB)}.");
-}
+else Console.Write($"{nmbrA} в {nmbrB} степени = {Exponentiation(nmbrA, nmbrB)}.");
 
 int Exponentiation(int nmbrA_value, int nmbrB_value)
 {
     int result = 1;
-    for (int i = 0; i < nmbrB_value; i++)
-    {
-        result *= nmbrA_value;
-    }
+    for (int i = 0; i < nmbrB_value; i++) result *= nmbrA_value;
     return result;
 }
