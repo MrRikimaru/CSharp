415993342fc976fe3af8badf7f40a1b433d1fed9
Berg Andrey <berg.andrey@icloud.com>
2023-03-06 23:00:08 +0300

    Add Task 27

---
+++ Task 27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число:");
+int nmbr = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{nmbr} -> {SumDigits(nmbr)}.");
+
+int SumDigits(int nmbr_value)
+{
+    int buffer = nmbr_value, sum = 0;
+    while (buffer > 0)
+    {
+        sum += buffer % 10; 
+        buffer /= 10;
+    }
+    return sum;
+}
